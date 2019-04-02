2474e0f70657aa62d64d4d750d7e3bba966eb4a8
Yahtnif <yahtnif@gmail.com>
2019-04-02 16:25:41 +0800

    Change license to 996ICU

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Ata
 
+[![996ICU Licence](<https://img.shields.io/badge/license-NPL%20(The%20996%20Prohibited%20License)-blue.svg>)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
+
 - axios + random useragent + encode url + ...
 - fetch style requests with `$` prefixed methods, like [axios-module](https://github.com/nuxt-community/axios-module)
 - support string proxy config like `127.0.0.1:9000`
@@ -28,3 +30,7 @@ async function go() {
 
 go()
 ```
+
+## License
+
+[996ICU Licence](./LICENSE)
--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const axios = require('axios')
-const randomMua = require('random-mua')
-const encodeUrl = require('encodeurl')
+const axios = require('axios');
+const randomMua = require('random-mua');
+const encodeUrl = require('encodeurl');
 
-const Ata = axios.create()
+const Ata = axios.create();
 
 Ata.interceptors.request.use(
   function(config) {
-    config.url = encodeUrl(config.url)
+    config.url = encodeUrl(config.url);
 
     if (!config.headers['User-Agent']) {
-      config.headers['User-Agent'] = randomMua()
+      config.headers['User-Agent'] = randomMua();
     }
 
     if (config.proxy && typeof config.proxy === 'string') {
-      const [host, port] = config.proxy.split(':')
+      const [host, port] = config.proxy.split(':');
       config.proxy = {
         host,
         port
-      }
+      };
     }
-    return config
+    return config;
   },
   function(error) {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 /**
  * Fork: [axios-module](https://github.com/nuxt-community/axios-module)
  */
-const shortcut = Object.create(null)
+const shortcut = Object.create(null);
 const requestMethods = [
   'request',
   'delete',
@@ -39,16 +39,17 @@ const requestMethods = [
   'post',
   'put',
   'patch'
-]
+];
 
 for (const method of requestMethods) {
   shortcut['$' + method] = function() {
-    return this[method].apply(this, arguments).then(res => res && res.data)
-  }
+    return this[method].apply(this, arguments).then(res => res && res.data);
+  };
 }
 
 for (const key in shortcut) {
-  Ata[key] = shortcut[key].bind(Ata)
+  Ata[key] = shortcut[key].bind(Ata);
 }
 
-module.exports = Ata
+module.exports = Ata;
+module.exports.default = Ata;
